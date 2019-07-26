8fa8db950561c435722236d3397d17a5893d98bb
Crash <info@sampx.ru>
2019-07-26 19:15:57 +0300

    ficks StringExperiments

--- 4 модуль/HomeWork 4.5/StringExperiments/src/Loader.java
+++ 4 модуль/HomeWork 4.5/StringExperiments/src/Loader.java
@@ -6,17 +6,13 @@ public class Loader
 {
     public static void main(String[] args)
     {
+        Pattern pattern = Pattern.compile("[0-9]+");
         String text = "Вася заработал 5000 рублей, Петя - 7563 рубля, а Маша - 30000 рублей";
-
-        System.out.println(text);
-        int payVasia = text.indexOf("Вася заработал");
-        int payVa = text.indexOf("руб");
-        String str1 = text.substring(payVasia+14, payVa).trim();
-
-        int payMasha = text.indexOf("Маша - ");
-        int payMa = text.lastIndexOf("руб");
-        String str2 = text.substring(payMasha+6, payMa).trim();
-        int symma=Integer.parseInt(str1)+Integer.parseInt(str2);
-        System.out.println("Сумма заработаных денег Маши и Васи состовляет: " + symma);
+        Matcher matcher = pattern.matcher(text);
+        int answer = 0;
+        while (matcher.find()) {
+            answer += Integer.parseInt(matcher.group());
+        }
+        System.out.println("В сумме у них получилось денег: " + answer);
     }
 }

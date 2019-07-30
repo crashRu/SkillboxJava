b3c9b4ff883f33a4ceb5d20fa27b45b545f7b987
Crash <info@sampx.ru>
2019-07-30 18:54:53 +0300

    Ficks 5.1

--- 5 модуль/HomeWork 5.1/experementX/src/Main.java
+++ 5 модуль/HomeWork 5.1/experementX/src/Main.java
@@ -1,20 +1,28 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
 public class Main {
-	public static void main(String[] args) {
-		String[][] array = {
-				{"x", " ", " ", " ", " ", "x"},
-				{" ", "x", " ", " ", "x",},
-				{" ", " ", "x", "x"},
-				{" ", " ", "  x "},
-				{" ", " ", "x", "x"},
-				{" ", "x", " ", "", " x"},
-				{"x", " ", " ", " ", " ", "x"},
-		};
+	public static void main(String[] args) throws IOException {
+		BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+		int sizeArray = Integer.parseInt(reader.readLine());
+		char[][] array = new char[sizeArray + 1][sizeArray + 1];
 
 		for (int i = 0; i < array.length; i++) {
-			for (int j = 0; j < array[i].length; j++) {
-				System.out.print(" " + array[i][j] + " ");
-			}
 			System.out.println();
+			for (int j = 0; j <=array.length; j++) {
+				if (i == j || j == array.length - i) {
+					array[i][j] = 'X';
+
+				} else if (i == array.length - i) {
+					array[i][j] = 'X';
+					break;
+
+				} else {
+					array[i][j] = ' ';
+				}
+				System.out.print(array[i][j]);
+			}
 		}
 	}
 }

1fb52f480bcf5ba68b1b02ecf56fdfbd783a17c4
Crash <info@sampx.ru>
2019-08-15 00:51:45 +0300

    add homework

--- 5 модуль/HomeWork 5.4/PhoneBook/src/Main.java
+++ 5 модуль/HomeWork 5.4/PhoneBook/src/Main.java
@@ -1,6 +1,4 @@
-import java.util.HashMap;
-import java.util.Map;
-import java.util.Scanner;
+import java.util.*;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -9,16 +7,18 @@ public class Main {
 		HashMap<String, String> phoneBook = new HashMap<>();
 		Scanner scanner = new Scanner(System.in);
 		Pattern checkNumber = Pattern.compile("^((8|\\+7)[\\- ]?)?(\\(?\\d{3}\\)?[\\- ]?)?[\\d\\- ]{7,10}$");
-		Pattern checkName = Pattern.compile("([А-Я][а-яё]{1,23})");
+		Pattern checkName = Pattern.compile("^([А-ЯA-Z]|[А-ЯA-Z][\\x27а-яa-z]{1,}|[А-ЯA-Z][\\x27а-яa-z]{1,}\\-([А-ЯA-Z][\\x27а-яa-z]{1,}|(оглы)|(кызы)))\\040[А-ЯA-Z][\\x27а-яa-z]{1,}(\\040[А-ЯA-Z][\\x27а-яa-z]{1,})?$");
 		while (true) {
 			System.out.println("Введите пожалуйста данные о абоненте!");
 			String text = scanner.nextLine();
 			if (text.equalsIgnoreCase("LIST")) {
-				PrintBook(phoneBook);
+				printBook(phoneBook);
 
 			} else if (checkNumber.matcher(text).find()) {
+				String formatNumber = "^([7|8])?(\\d{3})(\\d{3})(\\d{2})(\\d{2}$)";
+				text = text.replaceAll("\\D", "").replaceFirst(formatNumber, "+7 $2 $3-$4-$5");
 				if (phoneBook.containsValue(text)) {
-					System.out.println("Номер телефона: " + text + " Данные о клиенте: " + phoneBook.get(text));
+					System.out.println("Номер телефона: " + text + " Данные о клиенте: " + searchKey(phoneBook,text));
 					continue;
 				} else {
 					System.out.println("Введите пожалуйста имя абонента с номером " + text);
@@ -30,7 +30,7 @@ public class Main {
 				}
 			} else if (checkName.matcher(text).find()) {
 				if (phoneBook.containsKey(text)) {
-					System.out.println("Номер телефона: " + text + " Данные о клиенте: " + phoneBook.get(text));
+					System.out.println("Номер телефона: " + phoneBook.get(text) + " Данные о клиенте: " + text);
 				} else {
 					System.out.println("Введите пожалуйста номер абонента с именем " + text);
 					String number = scanner.nextLine();
@@ -47,9 +47,18 @@ public class Main {
 	}
 
 
-	private static void PrintBook(Map<String, String> map) {
+	private static void printBook(Map<String, String> map) {
 		for (String key : map.keySet()) {
-			System.out.println("Номер телефона :" + key + " Данные о клиенте: " + map.get(key));
+			System.out.println("Номер телефона :" + map.get(key) + " Данные о клиенте: " + key);
 		}
+	}
+
+	private static String searchKey(Map<String, String> map, String text) {
+		for (Map.Entry<String, String> pair : map.entrySet()) {
+			if (text.equals(pair.getValue())) {
+				return pair.getKey();
+			}
+		}
+		return "!";
 	}
 }
